52be0f828845b8586f81963cfdf2da6bc5c8545a
Yoan Mollard <ymollard@user.noreply.github.com>
2019-12-10 21:55:56 +0100

    Camera/Grasp pose

--- src/manipulate.py
+++ src/manipulate.py
@@ -3,17 +3,22 @@
 import rospy
 from moveit_commander.move_group import MoveGroupCommander
 from intera_interface.gripper import Gripper
+from intera_interface.camera import Cameras
 
 class ManipulateNode(object):
     def __init__(self):
         rospy.init_node("manipulate_sawyer")
         self.gripper = Gripper()
-        self.commander = MoveGroupCommander("right_arm")
+        #self.commander = MoveGroupCommander("right_arm")
+        self.camera = Cameras()
 
     def run(self):
         # Main function: actual behaviour of the robot
-        self.commander.set_pose_target([0.526, 0.477, 0.304, 0.702, -0.095, 0.705, 0.034])
-        self.go()
+        #self.commander.set_pose_target([0.526, 0.477, 0.304, 0.702, -0.095, 0.705, 0.034])
+        #self.go()
+        self.camera.set_cognex_strobe(False)
+        #self.camera.
+        #self.camera.set_cognex_strobe(False)
         self.gripper.close()
         rospy.sleep(5)
         self.gripper.open()
